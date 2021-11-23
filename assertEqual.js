b02919e6acf7676e79c6da07e84f44ae68700276
houseofsam <samxsaid@gmail.com>
2021-11-23 00:33:00 +0000

    created assertEqual function to check if 2 arguments passed into it match -- similar to console.assert, but with a more colourful and verbose output.

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual('Lighthouse Labs', 'Lighthouse Labs');
+assertEqual(' ', ' ');
+assertEqual(1, 1);
+assertEqual(1, 11);
