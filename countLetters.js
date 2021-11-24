5eb72c03689096a0f74c5dcf101f9f4084bbeada
houseofsam <samxsaid@gmail.com>
2021-11-24 23:59:40 +0000

    countLetters.js (not sure if should count uppercase letters separately and whether or not to include punctuation)

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(sentence) {
+  let letterCounter = {};
+
+  for (let letter of sentence) {
+    if (letter !== ' ') {
+      if (letterCounter[letter]) {
+        letterCounter[letter] += 1;
+      } else {
+        letterCounter[letter] = 1;
+      }
+    }
+  }
+
+  // console.log(letterCounter);
+  return letterCounter;
+};
+
+
+countLetters('Hello I\'m a student at LHL');
+countLetters("lighthouse in the house");
+countLetters('');
+// countLetters(43); throws error 
+
+
+// not sure whether I should count capital and lowercase letters as one. 
+// Also not sure if I should include punctuation.
+// I can most def use regex here. 
