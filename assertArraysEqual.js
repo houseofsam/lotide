dac67e3709ad59abc4fcc76757719bd6e2f91104
houseofsam <samxsaid@gmail.com>
2021-11-24 05:00:18 +0000

    modified asserArraysEqual, eqArrays and without.js after mentor talk

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,10 @@
 const eqArrays = function(arr1, arr2) {
-  let perfectMatch = true;
-
-  // verify that inputs are arrays and that they are the same length. If even one element differs from the other, identify it as a false match.
-  if ((Array.isArray(arr1) && Array.isArray(arr2)) && (arr1.length === arr2.length)) {
-    for (let i = 0; i < arr1.length; i++) {
-      arr1[i] !== arr2[i] ? perfectMatch = false : '';
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
-  } else {
-    // if the arrays do not match the conditions in the above if statement, identify them as false.
-    perfectMatch = false;
   }
-  // console.log(perfectMatch);
-  return perfectMatch;
+  return true;
 };
 
 const assertArraysEqual = function(actualArr, expectedArr) {
--- eqArrays.js
+++ eqArrays.js
@@ -5,19 +5,12 @@ const assertEqual = function(actual, expected) {
 // Method 2 - Looping and comparing each array item index to each other
 
 const eqArrays = function(arr1, arr2) {
-  let perfectMatch = true;
-
-  // verify that inputs are arrays and that they are the same length. If even one element differs from the other, identify it as a false match.
-  if ((Array.isArray(arr1) && Array.isArray(arr2)) && (arr1.length === arr2.length)) {
-    for (let i = 0; i < arr1.length; i++) {
-      arr1[i] !== arr2[i] ? perfectMatch = false : '';
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
-  } else {
-    // if the arrays do not match the conditions in the above if statement, identify them as false.
-    perfectMatch = false;
   }
-  // console.log(perfectMatch);
-  return perfectMatch;
+  return true;
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
