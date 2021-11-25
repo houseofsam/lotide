d60f67260735fb583be6918ce08eab5bbe8ad725
houseofsam <samxsaid@gmail.com>
2021-11-24 22:24:49 -0500

    middle.js  I might have to review this and find a better approach. spent a lot of time figuring out why assertArraysEqual wasn't working properly. 1) had to compare array lengths in eqArrays otherwise if arr1.length is 1, it'll return a false positive. 2) odd conditional was returning a single number instead of an array consisting of 1 number

---
+++ middle.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  // console.log(arr1, arr2)
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length || arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actualArr, expectedArr) {
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }  
+};
+
+const oddGetMiddleIndex = function(indexes) {
+  return Math.floor(indexes.length / 2);
+}
+
+const middle = function(arr) {
+  let output = [];
+
+  if (arr.length <= 2) {
+    return []
+  } else if (!(arr.length % 2)) {
+    output.push(arr[oddGetMiddleIndex(arr) - 1]);
+    output.push(arr[oddGetMiddleIndex(arr)]);
+  } else {
+    output = [arr[oddGetMiddleIndex(arr)]];
+  }
+  // console.log(output);
+  return output;
+}
+
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([10, 20, 30]), [20]);
+assertArraysEqual(middle([1, 2, 30, 4, 5]), [30]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
