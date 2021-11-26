4abeba6b5573cbe669c7a8ad9d6ae57543060ce1
houseofsam <samxsaid@gmail.com>
2021-11-25 23:24:12 -0500

    findKey.js & takeUntil.js

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function (object, callback) {
+  for (let property in object) {
+    if (callback(object[property])) {
+      // console.log(property);
+      return property;
+    }
+  }
+};
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+result1;
+
+assertEqual(result1, 'noma');
