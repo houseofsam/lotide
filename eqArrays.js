9a35946fcf8f757763ff0a87022758517676bc98
houseofsam <samxsaid@gmail.com>
2021-11-29 22:51:00 +0000

    eqArrays and eqArraysTest refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,3 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 // Method 2 - Looping and comparing each array item index to each other
 
 const eqArrays = function(arr1, arr2) {
@@ -13,10 +9,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
