51c93b4bb9f1dac1ded00827a758e51e9490074e
houseofsam <samxsaid@gmail.com>
2021-11-29 22:59:55 +0000

    assertArraysEqual refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length || (arr1[i] !== arr2[i])) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actualArr, expectedArr) {
   if (eqArrays(actualArr, expectedArr)) {
-  console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
+    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
-  }  
+  }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [1,'2',3]);
-assertArraysEqual([], []);
+module.exports = assertArraysEqual;
