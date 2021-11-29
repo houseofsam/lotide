a1c643778b23c86f39be1b0b44da5b048c88087d
houseofsam <samxsaid@gmail.com>
2021-11-29 22:17:26 +0000

    begin lotide refactoring. exported and removed test cases from assertEqual & head.js. Imported them via require where needed. Moved test cases to sepearate files in test folder.

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } 
 };
 
-//TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual('Lighthouse Labs', 'Lighthouse Labs');
-assertEqual(' ', ' ');
-assertEqual(1, 1);
-assertEqual(1, 11);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+// const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr.length > 0 ? arr[0] : undefined;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 9);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([5]), 6);
-assertEqual(head([]), undefined);
+module.exports = head;
