2220950b5471724bfc53d868e9b9786cef3a8720
houseofsam <samxsaid@gmail.com>
2021-11-30 01:59:39 +0000

    created index.js and imported head, tail, middle, assertEqual, assertArraysEqual lotide functions.

---
+++ index.js
@@ -0,0 +1,17 @@
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+// To-do
+
+module.exports = {
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  head: head,
+  tail: tail,
+  middle: middle
+
+};
+
