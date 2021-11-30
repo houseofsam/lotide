ef8f20ced27101788001e98019f7d29acff0e067
houseofsam <samxsaid@gmail.com>
2021-11-30 00:14:58 +0000

    tailTest.js refactored. Mocha/Chai implemented

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,32 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([1, 4, 10]).join(' '), [4, 10].join(' '));
-assertEqual(tail(['lighthouse', 'labs', 'east', 'west']).join(' '), ['labs', 'east', 'west'].join(' '));
-assertEqual(tail(['test'].join(' ')), [].join(' '));
-assertEqual(tail([1].join(' ')), [].join(' '));
-assertEqual(tail([]).join(' '),[].join(' '));
+describe('#tailTest', () => {
+  it('returns [4, 10] for [1, 4, 10]', () => {
+    assert.deepEqual(tail([1, 4, 10]), [4, 10]);
+  });
+  
+  it('returns ["labs, "east", "west"] for ["lighthouse", "labs", "east", "west"]', () => {
+    assert.deepEqual(tail(["lighthouse", "labs", "east", "west"]), ["labs", "east", "west"]);
+  });
 
+  it('returns [] for ["test"]', () => {
+    assert.deepEqual(tail(["test"]), []);
+  });
 
-// Verify that the tail function is returning a new array and not modifying the original array that is passed in.
-const words = ["hey", "its", "me"];
-tail(words);
-assertEqual(words.length, 3);
-// assertEqual(tail(words).join(' '), ["its", "me"].join(' '));
+  it('returns [] for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it('Verify that the tail function is returning a new array and not modifying the original array that is passed in.', () => {
+    const words = ["hey", "its", "me"];
+    tail(words);
+    assert.strictEqual(tail(words).length, 2);
+    assert.strictEqual(words.length, 3);
+    assert.deepEqual(tail(words), ["its", "me"]);
+  });
+});
