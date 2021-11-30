12c9e7bf3eb98676729e5551f189451d13a5f675
houseofsam <samxsaid@gmail.com>
2021-11-30 02:04:32 +0000

    changed index.js to utilize object property shorthand

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const tail = require('./tail');
 const middle = require('./middle');
 
 // To-do
+// ('./flatten.js);
+// ('./countOnly.js);
+// ('./letterPositions.js);
+// ('./findKeyByValue.js);
+// ('./eqObjects.js);
+// ('./eqArrays.js);
 
 module.exports = {
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  head: head,
-  tail: tail,
-  middle: middle
-
+  assertEqual,
+  assertArraysEqual,
+  head,
+  tail,
+  middle
 };
 
